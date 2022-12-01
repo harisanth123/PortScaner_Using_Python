15219241724e1259441aeddc90207669552076b5
Harisanth P S <83811783+harisanth123@users.noreply.github.com>
2022-12-01 14:59:54 +0530

    banner added

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from socket import *
+from datetime import *
 import time
+import pyfiglet
+
 startTime = time.time()
 
 if __name__ == '__main__':
+   ascii_banner = pyfiglet.figlet_format("PORT SCANNER")
+   print(ascii_banner)	
    target = input('Enter the host to be scanned: ')
    t_IP = gethostbyname(target)
    print ('Starting scan on host: ', t_IP)
+   print("Scanning started at:" + str(datetime.now()))
    
    for i in range(50, 500):
       s = socket(AF_INET, SOCK_STREAM)
