10c25abee5b2a60ec3c85a491be43465f29eb653
Harisanth P S <83811783+harisanth123@users.noreply.github.com>
2022-12-01 14:48:28 +0530

    main.py

--- main.py
+++ main.py
@@ -1,44 +1,17 @@
-import pyfiglet
-import sys
-import socket
-from datetime import datetime
+from socket import *
+import time
+startTime = time.time()
 
-ascii_banner = pyfiglet.figlet_format("PORT SCANNER")
-print(ascii_banner)
-
-# Defining a target
-if len(sys.argv) == 2:
-	
-	# translate hostname to IPv4
-	target = socket.gethostbyname(sys.argv[1])
-else:
-	print("Invalid amount of Argument")
-
-# Add Banner
-print("-" * 50)
-print("Scanning Target: " + target)
-print("Scanning started at:" + str(datetime.now()))
-print("-" * 50)
-
-try:
-	
-	# will scan ports between 1 to 65,535
-	for port in range(1,65535):
-		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		socket.setdefaulttimeout(1)
-		
-		# returns an error indicator
-		result = s.connect_ex((target,port))
-		if result ==0:
-			print("Port {} is open".format(port))
-		s.close()
-		
-except KeyboardInterrupt:
-		print("\n Exiting Program !!!!")
-		sys.exit()
-except socket.gaierror:
-		print("\n Hostname Could Not Be Resolved !!!!")
-		sys.exit()
-except socket.error:
-		print("\ Server not responding !!!!")
-		sys.exit()
+if __name__ == '__main__':
+   target = input('Enter the host to be scanned: ')
+   t_IP = gethostbyname(target)
+   print ('Starting scan on host: ', t_IP)
+   
+   for i in range(50, 500):
+      s = socket(AF_INET, SOCK_STREAM)
+      
+      conn = s.connect_ex((t_IP, i))
+      if(conn == 0) :
+         print ('Port %d: OPEN' % (i,))
+      s.close()
+print('Time taken:', time.time() - startTime)
